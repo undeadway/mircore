598567b5d1475de66a2c88ada9f2007254e16cc7
undeadway <undeadway@163.com>
2020-07-07 09:09:36 +0800

    修改文件夹结构

---
+++ demo/index.js
@@ -0,0 +1,2 @@
+let server = require("../src/index");
+server.start();
---
+++ demo/src/modules/index/controller.js
@@ -0,0 +1,10 @@
+const controller = require("./../../../../src/core/controller");
+
+function indexController() {
+
+    const ctrler = controller();
+
+    return ctrler;
+}
+
+module.exports = exports = indexController;
