99429a991f50ecb4a481867c9b2c1f2aec3964bb
undeadway <undeadway@163.com>
2020-07-12 16:03:17 +0800

    修改 controller 定义

--- test/src/modules/method/controller.js
+++ test/src/modules/method/controller.js
@@ -4,10 +4,15 @@ const { getLink } = require("./../../util/util");
 const { HttpRequestMethod } = Coralian.constants;
 const PAGE = "/res/html/method.html"
 
-function indexAction() {
+function getAction() {
 	const action = baseAction();
 
 	action.execute = () => {
+
+		const ctrler = action.controller;
+
+		let param = ctrler.getQuery("param");
+		ctrler.setAttr("param", param);
 
 		ctrler.render(PAGE);
 	};
@@ -15,11 +20,26 @@ function indexAction() {
 	return action;
 }
 
+function postAction() {
+
+}
+
+function putAction() {
+
+}
+
+function deleteAction () {
+
+}
+
 function indexController() {
 
 	const ctrler = controller();
 
-	ctrler.addAction(indexAction, HttpRequestMethod.GET);
+	ctrler.addAction("index", getAction, HttpRequestMethod.GET);
+	ctrler.addAction("index", postAction, HttpRequestMethod.POST);
+	ctrler.addAction("index", putAction, HttpRequestMethod.PUT);
+	ctrler.addAction("index", deleteAction, HttpRequestMethod.DELETE);
 
 	return ctrler;
 }
