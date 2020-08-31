1d25eb6307d32ac827958b87dca9369b4218edd4
undeadway <undeadway@163.com>
2020-08-31 10:44:59 +0800

    双引号化处理

--- src/core/server.js
+++ src/core/server.js
@@ -5,7 +5,6 @@
  * 也就是说，server 这里要完成的是 nodejs 没有实现，但是整个应用程序却需要的功能
  * 更接近于服务器的设置
  */
-
 const url = require("url"),
 	qs = require("querystring");
 
@@ -14,7 +13,7 @@ const cookieNewInstance = require("../server/cookies"),
 const { port, appName, developMode, clusterMode } = require("../util/app-config");
 const { clientDisAccessable } = require("../util/utils").privates;
 
-const { HttpStatusCode, HttpRequestMethod } = Coralian.constants;
+const { HttpStatusCode, HttpRequestMethod, Mark } = Coralian.constants;
 const { formatString } = Coralian.Formatter;
 const { unsupportedOperation } = Error;
 const TIMEOUT = 20000,
@@ -25,10 +24,9 @@ let isStarted = false;
  * 初始化完毕，执行 listen 函数启动 http 服务器
  * 因为 nodejs实现了 http 服务器，所以不用自己实现相关功能
  * 这个函数只会在初始化服务器的时候才会调用一次
- * 
  */
 function listen(name) {
-	let httpServer = require('http').createServer(router);
+	let httpServer = require("http").createServer(router);
 	httpServer.listen(port);
 	Coralian.logger.log(`${name} Server started`);
 }
@@ -43,8 +41,8 @@ function router(req, res) {
 	let parse = req.parse = url.parse(req.url, true);
 	let method = req.method = req.method.toUpperCase();
 
-	let _postData = '';
-	req.setEncoding('utf8');
+	let _postData = String.BLANK;
+	req.setEncoding("utf8");
 
 	setClientInfo(req);
 
@@ -54,7 +52,7 @@ function router(req, res) {
 		return;
 	} else {
 		req.on(Error.TYPE_NAME, onError)
-			.on('data', function (chunk) {
+			.on("data", function (chunk) {
 				// TODO 现在这里只处理 post 上来的字符串，二进制格式要怎么弄还要再研究
 				_postData += chunk;
 			}).on("end", function () {
@@ -80,7 +78,7 @@ function router(req, res) {
 				if (developMode) return; // 开发模式的情况下，无视各种超时
 				// req 请求超时，网络不稳定
 				// 408 Request Timeout
-				Coralian.logger.err('request 请求错误: ' + HttpStatusCode.REQUEST_TIMEOUT);
+				Coralian.logger.err(`request 请求错误: ${HttpStatusCode.REQUEST_TIMEOUT}`);
 				req.url = formatString(ERROR_ROUTE_FORMAT, HttpStatusCode.REQUEST_TIMEOUT);
 				req.parse = url.parse(req.url, true);
 				request(req, res);
@@ -90,7 +88,7 @@ function router(req, res) {
 				if (developMode) return; // 开发模式的情况下，无视各种超时
 				// res 响应超时，服务器无应答
 				// 504 Gateway Timeout
-				Coralian.logger.err('response 返信错误 : ' + HttpStatusCode.GATEWAY_TIMEOUT);
+				Coralian.logger.err(`response 返信错误: ${HttpStatusCode.REQUEST_TIMEOUT}`);
 				req.url = formatString(ERROR_ROUTE_FORMAT, HttpStatusCode.GATEWAY_TIMEOUT);
 				parse = url.parse(req.url, true);
 				request(req, res);
@@ -105,12 +103,12 @@ function router(req, res) {
 	 */
 	function onError(err) {
 		// 请求（或响应）发生错误的时候，如果内部任何一层都未能捕获，则在最外层捕获
-		Coralian.logger.err("HTTP req error : ");
+		Coralian.logger.err("HTTP request error : ");
 		Coralian.logger.err(err.code);
 		Coralian.logger.err(err.stack);
 
 		// 如果发生 write after end ，则只将错误信息记录到 log 中去
-		if (String.contains(err.message, 'write after end')) return;
+		if (String.contains(err.message, "write after end")) return;
 
 		req.url = formatString(ERROR_ROUTE_FORMAT, (err.code || HttpStatusCode.INTERNAL_SERVER_ERROR));
 		req.parse = url.parse(req.url, true);
@@ -150,42 +148,42 @@ function initUserAgendAndOS(headers, client) {
 	let input = getUserAgent(headers);
 	let userAgent, os;
 
-	if (String.contains(input, 'windows')) {
-		os = 'Windows';
-	} else if (String.contains(input, 'linux')) {
-		os = 'Linux';
+	if (String.contains(input, "windows")) {
+		os = "Windows";
+	} else if (String.contains(input, "linux")) {
+		os = "Linux";
 	} else if (String.contains(input, "sun") || String.contains(input, "solaris")) {
-		os = 'Solaris'
-	} else if (String.contains(input, 'android')) {
-		os = 'Android';
-	} else if (String.contains(input, 'iphone') || String.contains(input, 'ipad') || String.contains(input, "apple")) {
-		os = 'iOS';
+		os = "Solaris"
+	} else if (String.contains(input, "android")) {
+		os = "Android";
+	} else if (String.contains(input, "iphone") || String.contains(input, "ipad") || String.contains(input, "apple")) {
+		os = "iOS";
 	} else {
-		os = 'Unknown';
+		os = "Unknown";
 	}
 
 	if (String.contains(input, "trident") || String.contains(input, "microsoft") ||
-		(String.contains(input, "ie") && os === 'Windows')) // 为避免 ua 中含有 “ie” 字符串造成的歧义，加上操作系统名限制
+		(String.contains(input, "ie") && os === "Windows")) // 为避免 ua 中含有 “ie” 字符串造成的歧义，加上操作系统名限制
 	{
-		userAgent = 'IE';
-	} else if (String.contains(input, 'edge')) {
-		userAgent = 'Edge';
-	} else if (String.contains(input, 'micromessenger') || String.contains(input, 'micromsg') || String.contains(input, 'weixin') || String.contains(input, 'wechat')) {
-		userAgent = 'Wechat';
-	} else if (String.contains(input, 'opr')) {
-		userAgent = 'Opera';
-	} else if (String.contains(input, 'chrome')) {
-		userAgent = 'Chrome';
-	} else if (String.contains(input, 'safari')) {
-		userAgent = 'Safari';
+		userAgent = "IE";
+	} else if (String.contains(input, "edge")) {
+		userAgent = "Edge";
+	} else if (String.contains(input, "micromessenger") || String.contains(input, "micromsg") || String.contains(input, "weixin") || String.contains(input, "wechat")) {
+		userAgent = "Wechat";
+	} else if (String.contains(input, "opr")) {
+		userAgent = "Opera";
+	} else if (String.contains(input, "chrome")) {
+		userAgent = "Chrome";
+	} else if (String.contains(input, "safari")) {
+		userAgent = "Safari";
 	} else if (String.contains(input, "apple")) {
 		userAgent = "Safari";
-	} else if (String.contains(input, 'gekko') || String.contains(input, 'mozilla')) {
-		userAgent = 'Firefox';
+	} else if (String.contains(input, "gekko") || String.contains(input, "mozilla")) {
+		userAgent = "Firefox";
 	} else if (String.contains(input, "curl")) {
 		userAgent = "curl";
 	} else {
-		userAgent = 'Unknown';
+		userAgent = "Unknown";
 	}
 
 	client.USERAGENT = userAgent;
@@ -196,7 +194,7 @@ function initUserAgendAndOS(headers, client) {
 }
 
 function initClientIp(req, client) {
-	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||
+	let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.socket.remoteAddress ||
 		req.connection.socket.remoteAddress;
 	client.IP = ip;
 
@@ -209,9 +207,9 @@ function initClientIp(req, client) {
  */
 function getUserAgent(header) {
 
-	let userAgent = header['user-agent'];
+	let userAgent = header["user-agent"];
 
-	userAgent = userAgent || 'unknown';
+	userAgent = userAgent || "unknown";
 	return userAgent.toLowerCase();
 }
 
@@ -239,46 +237,46 @@ module.exports = exports = () => {
 		 * 这段代码当时是从网上抄来的，据说对服务器稳定有好处
 		 * 但从实际运行来看，好像没什么变化，暂时保留
 		 */
-		let cluster = require('cluster');
+		let cluster = require("cluster");
 
 		if (cluster.isMaster) {
-			process.title = appName + ' master';
-			Coralian.logger.log(process.title, '#' + process.pid, 'started');
+			process.title = `${appName} master`;
+			Coralian.logger.log(process.title, `${Mark.SHARP}${process.pid}`, "started");
 
 			// 根据 CPU 个数来启动相应数量的 worker
-			for (let i = 0, numCPUs = require('os').cpus().length; i < numCPUs; i++) {
+			for (let i = 0, numCPUs = require("os").cpus().length; i < numCPUs; i++) {
 				cluster.fork();
 			}
 
-			process.on('SIGHUP', function () {
+			process.on("SIGHUP", function () {
 				// master 进程忽略 SIGHUP 信号
 			});
 
-			cluster.on('online', function (worker) {
-				Coralian.logger.log('Worker ' + worker.process.pid + ' is online');
+			cluster.on("online", function (worker) {
+				Coralian.logger.log(`Worker ${worker.process.pid} is online`);
 			});
 
-			cluster.on('exit', function (worker, code, signal) {
-				Coralian.logger.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-				Coralian.logger.log('Starting a new worker');
+			cluster.on("exit", function (worker, code, signal) {
+				Coralian.logger.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+				Coralian.logger.log("Starting a new worker");
 				cluster.fork();
 			});
 
-			cluster.on('death', function (worker) {
-				Coralian.logger.log(appName, 'worker', '#' + worker.pid, 'died');
+			cluster.on("death", function (worker) {
+				Coralian.logger.log(appName, "worker",`${Mark.SHARP}${worker.pid}`, "died");
 				cluster.fork();
-			}).on('error', function () {
+			}).on("error", function () {
 				Coralian.logger.log(arguments);
 			});
 		} else {
-			process.title = appName + ' worker ' + process.env.NODE_WORKER_ID;
+			process.title = `${appName} worker ${process.env.NODE_WORKER_ID}`;
 
-			process.on('SIGHUP', function () {
+			process.on("SIGHUP", function () {
 				// 接收到 SIGHUP 信号时，关闭 worker
 				process.exit(0);
 			});
 
-			listen(process.title, '#' + process.pid);
+			listen(process.title, `${Mark.SHARP}${process.pid}`);
 		}
 	} else {
 		listen(appName); // 开发模式或者非集群模式下，简化所有配置，直接启动服务器
