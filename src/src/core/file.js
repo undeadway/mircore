c4ec120b2f66b3de75af29e72a2ee6fbd240d7d3
waygc <hzwaygc@gmail.com>
2021-08-06 09:05:41 +0800

    调整结构

---
+++ src/src/core/file.js
@@ -0,0 +1,21 @@
+/**
+ * 用于处理上传的文件
+ */
+
+function file () {
+    return {
+        saveTo: () => {
+
+        },
+        toBase64: () => {
+
+        }
+    }
+}
+
+
+module.exports = {
+    init: () => {
+
+    }
+};
