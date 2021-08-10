9704eae42af9193d7636f66e09dd0237189f32f8
waygc <hzwaygc@gmail.com>
2021-08-10 15:35:14 +0800

    显示所有提交数据

--- demo/src/modules/file/controller.js
+++ demo/src/modules/file/controller.js
@@ -7,12 +7,22 @@ function nonoController() {
 
 	ctrler.addAction(() => {
 
-		let fd = ctrler.getFile("data1");
+		let fd1 = ctrler.getFile("data1");
 
-		let data = fd.getBase64Data();
-		let base64 = `data:${data}`;
-		ctrler.setAttr("hash", fd.getHash());
-		ctrler.setAttr("base64", base64);
+		let data1 = fd1.getBase64Data();
+		let base641 = `data:${data1}`;
+		ctrler.setAttr("hash1", fd1.getHash());
+		ctrler.setAttr("base641", base641);
+
+		let fd2 = ctrler.getFile("data2");
+
+		let data2 = fd2.getBase64Data();
+		let base642 = `data:${data2}`;
+		ctrler.setAttr("hash2", fd2.getHash());
+		ctrler.setAttr("base642", base642);
+
+		ctrler.setAttr("txt1", ctrler.getQuery("txt1"));
+		ctrler.setAttr("txt2", ctrler.getQuery("txt2"));
 
 		ctrler.render({url: PAGE});
 	}, "post");
