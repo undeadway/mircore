e58394fc8313a3136d42e3bbdb5b7f900a3a6841
waygc <hzwaygc@gmail.com>
2021-08-18 11:06:51 +0800

    修改测试

--- demo/src/modules/image/controller.js
+++ demo/src/modules/image/controller.js
@@ -1,14 +1,20 @@
 const { controller, file } = mircore;
-const file_name = "/home/waygc/workspace/mircore/demo/res/file/mylogo.svg";
+const file_name_svg = "/home/waygc/workspace/mircore/demo/res/file/mylogo.svg";
+const file_name_png = "/home/waygc/workspace/mircore/demo/res/file/mylogo.png";
 
 function nonoController() {
 
 	const ctrler = controller();
 
-	ctrler.addAction(() => {
+	ctrler.addAction("svg", () => {
 
-		ctrler.renderFile(file_name);
+		ctrler.renderFile(file_name_svg);
 	});
+	ctrler.addAction("png", () => {
+
+		ctrler.renderFile(file_name_png);
+	});
+
 
 	return ctrler;
 }
