0ed13147e3467493870237f05187d877bfde9934
waygc <hzwaygc@gmail.com>
2022-10-31 13:26:59 +0800

    修复系统组件

--- src/core/server.js
+++ src/core/server.js
@@ -5,6 +5,9 @@
  * 也就是说， server 这里要完成的是 nodejs 没有实现，但是整个应用程序却需要的功能
  * 更接近于服务器的设置
  */
+// 系统组件
+const url = require("url");
+
 // mircore 的组件
 const Cookies = require("../components/public/cookies");
 const parseRequest = require("../components/private/parse-request"),
